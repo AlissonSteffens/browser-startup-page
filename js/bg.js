125c991e375ff09f142ba3cb1d03b72901f11640
Alisson Steffens <ali.steffens@gmail.com>
2020-11-20 13:39:44 -0300

    :art: new features

--- js/bg.js
+++ js/bg.js
@@ -1,3 +1,6 @@
+var myData = ""
+var lastPallet = ""
+
 const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => {
   const hex = x.toString(16)
   return hex.length === 1 ? '0' + hex : hex
@@ -27,112 +30,172 @@ function getColorLuminosity(color) {
 function lightOrDark(color) {
   let hsp = getColorLuminosity(color);
   if (hsp > 127.5) {
-
     return 'light';
   }
   else {
-
     return 'dark';
   }
 }
 
 function getCssValuePrefix() {
-  var rtrnVal = '';//default to standard syntax
+  var rtrnVal = '';
   var prefixes = ['-o-', '-ms-', '-moz-', '-webkit-'];
-
-  // Create a temporary DOM object for testing
   var dom = document.createElement('div');
-
   for (var i = 0; i < prefixes.length; i++) {
-    // Attempt to set the style
     dom.style.background = prefixes[i] + 'linear-gradient(#000000, #ffffff)';
-
-    // Detect if the style was successfully set
     if (dom.style.background) {
       rtrnVal = prefixes[i];
     }
   }
-
   dom = null;
   delete dom;
-
   return rtrnVal;
 }
 
-function setImage(primaryImage, title, artist, objectDate, objectURL) {
-  document.getElementById("canvas").src = primaryImage;
-  // document.getElementById("overlay").style.backgroundImage = "url('"+item.primaryImageSmall+"')"
+function getColorasHex(a){
+  return rgbToHex(a[0], a[1], a[2]);
+}
+
+function placeMainImage(primaryImage){
+  if(image_place == 'framed'){
+    document.getElementById("canvas").style.display = 'block';
+    document.getElementById("canvas").src = primaryImage;
+  }else if(image_place == 'background'){
+    document.getElementById("canvas").style.display = 'none';
+    document.getElementById("bg").style.backgroundImage = 'url('+primaryImage+')';
+    document.getElementById("bg").style.backgroundSize = 'cover';
+  }else if(image_place == 'background-repeat'){
+    document.getElementById("canvas").style.display = 'none';
+    document.getElementById("bg").style.backgroundImage = 'url('+primaryImage+')';
+    document.getElementById("bg").style.backgroundSize = 'auto';
+  }
+}
+
+function setTexts(title, artist, objectDate, objectURL){
   document.getElementById("title").innerHTML = title;
   var desc = artist;
   if (objectDate != "") {
     desc += ' (' + objectDate + ') ';
   }
   if (objectURL != "") {
-
     desc += ' <a href="' + objectURL + '" target="_blank">+</a>';
-
   }
   document.getElementById("author").innerHTML = desc;
-
-  const colorThief = new ColorThief();
-
-  const img = new Image();
-  // const img = document.getElementById("canvas");
-  
-  img.crossOrigin = 'Anonymous';
-  img.src =  primaryImage;
-  img.src =  primaryImage;
-
-  img.addEventListener('load', () => {
-    var p = colorThief.getPalette(img, 4);
-    var a = p[0];
-    var c = rgbToHex(a[0], a[1], a[2]);
-    var b = p[1];
-    var c2 = rgbToHex(b[0], b[1], b[2]);
-
-    let l1 = getColorLuminosity(c);
-    let l2 = getColorLuminosity(c2);
-
-    document.getElementById("bg").style.backgroundImage = getCssValuePrefix() + 'radial-gradient(circle,' + (l1 < l2 ? c2 : c) + ', ' + (l1 < l2 ? c : c2) + ')';
-    var a = p[2];
-    var c = rgbToHex(a[0], a[1], a[2]);
-
-    document.getElementById("title").style.color = c;
-    var light = lightOrDark(c) == 'light' ? true : false;
-
-    if (light) {
-      document.getElementById("title").style.backgroundColor = "#000";
-    } else {
-      document.getElementById("title").style.backgroundColor = "#fff";
-    }
-    var a = p[3];
-    var c = rgbToHex(a[0], a[1], a[2]);
-    document.getElementById("author").style.backgroundColor = c;
-    var light = lightOrDark(c) == 'light' ? true : false;
-
-    if (light) {
-      document.getElementById("author").style.color = "#000";
-    } else {
-      document.getElementById("author").style.color = "#fff";
-    }
-    document.getElementById("overlay").style.display = 'none';
-  });
-
 }
 
-function doBG() {
+function getPallete(primaryImage, n, callback){
+  const colorThief = new ColorThief();
+  const img = new Image();
+  img.crossOrigin = 'Anonymous';
+  img.src =  primaryImage;
+  img.addEventListener('load', () => {
+    var p = colorThief.getPalette(img, n);
+    callback(p)
+  });
+}
+
+function getPalleteSize(){
+  if(image_place == 'framed'){
+    return 4;
+  }else if(image_place == 'background' || image_place == 'background-repeat'){
+    if(endpoint_images == 'pokemon'){
+      return 3;
+    }else{
+      return 2;
+    }    
+  }
+}
+
+function setBGColor(p){
+  if(image_place == 'framed'){
+    var c = getColorasHex(p[0]);
+    var c2 = getColorasHex(p[1]);
+    let l1 = getColorLuminosity(c);
+    let l2 = getColorLuminosity(c2);
+    document.getElementById("bg").style.backgroundImage = getCssValuePrefix() + 'radial-gradient(circle,' + (l1 < l2 ? c2 : c) + ', ' + (l1 < l2 ? c : c2) + ')';
+  }else if(image_place == 'background' || image_place == 'background-repeat'){
+    if(endpoint_images == 'pokemon'){
+      var c = getColorasHex(p[0]);
+      document.getElementById("bg").style.backgroundColor= c;
+    }
+  }
+}
+
+function blackOrWhite(c,tag,prop){
+  var light = lightOrDark(c) == 'light' ? true : false;
+  
+  if(prop == 'bg'){
+    document.getElementById(tag).style.backgroundColor = light?"#000":"#fff";
+  }
+  if(prop == 'color'){
+    document.getElementById(tag).style.color = light?"#000":"#fff";
+  }  
+}
+
+function setStyle(c1,c2){
+  var c = getColorasHex(c1);
+  document.getElementById("title").style.color = c;
+  blackOrWhite(c,'title','bg')
+  
+  var c = getColorasHex(c2);
+  document.getElementById("author").style.backgroundColor = c;
+  blackOrWhite(c,'author','color')
+}
+
+function setTextStyles(p){
+  if(image_place == 'framed'){
+    setStyle(p[2],p[3])
+    
+  }else if(image_place == 'background' || image_place == 'background-repeat'){
+    if(endpoint_images == 'pokemon'){
+      setStyle(p[1],p[2])
+    }else{
+      setStyle(p[0],p[1])
+    }
+  }
+}
+
+function setImageAndStyles(primaryImage, title, artist, objectDate, objectURL) {
+  placeMainImage(primaryImage)
+  setTexts(title, artist, objectDate, objectURL)
+  
+  getPallete(primaryImage, getPalleteSize(), (p) =>{
+    lastPallet = p
+    setBGColor(p)
+    setTextStyles(p)
+    document.getElementById("overlay").style.display = 'none';
+  });
+}
+
+
+function getDataAndDoBG() {
   document.getElementById("overlay").style.display = 'flex';
+  if(lastPallet != ""){
+    document.getElementById("overlay").style.backgroundColor = getColorasHex(lastPallet[0]);
+    document.getElementById("spinner-span").style.backgroundColor = getColorasHex(lastPallet[0]);
+    document.getElementById("spinner").style.background = getCssValuePrefix() + 'linear-gradient(0deg,'+getColorasHex(lastPallet[0])+' 33%, '+getColorasHex(lastPallet[1])+' 100%)';
+    blackOrWhite(getColorasHex(lastPallet[0]),'loading-text','color')
+  }
   const myRequest = new Request('https://awesome-api.vercel.app/api/'+endpoint_images);
   fetch(myRequest)
     .then(response => response.json())
     .then(json => {
+      myData = json;
       let dado1 = json.author || json.type;
       let dado2 = json.year || json.status || json.id;
       let dado3 = json.title || json.name;
-      setImage(json.img, dado3, dado1, dado2, json.source)
+      setImageAndStyles(json.img, dado3, dado1, dado2, json.source)
     })
 
 }
 
+function doBG() {
+  let dado1 = myData.author || myData.type;
+  let dado2 = myData.year || myData.status || myData.id;
+  let dado3 = myData.title || myData.name;
+  setImageAndStyles(myData.img, dado3, dado1, dado2, myData.source)
 
-doBG();
+}
+
+
+getDataAndDoBG();
--- js/configs.js
+++ js/configs.js
@@ -1,5 +1,7 @@
 var endpoint_images = localStorage.getItem("source") || 'art'
+var image_place = localStorage.getItem("image-place") || 'framed'
 document.getElementById('sources').value = endpoint_images;
+document.getElementById('image-place').value = image_place;
 function openConfigs(){
     document.getElementById("settings").style.display = 'flex';
 }
@@ -11,5 +13,13 @@ function closeConfigs(){
 function trocar_endpoint_imagem(){
     endpoint_images = document.getElementById("sources").value;
     localStorage.setItem("source", endpoint_images);
+    getDataAndDoBG()
+    closeConfigs()
+}
+
+function trocar_posicao_imagem(){
+    image_place = document.getElementById("image-place").value;
+    localStorage.setItem("image-place", image_place);
     doBG()
+    closeConfigs()
 }
