ae2b00defba28d2cd848b0840765d25837d7be76
Alisson Steffens <ali.steffens@gmail.com>
2021-04-14 01:51:30 -0300

    :bug: fix undefined test

--- js/bg.js
+++ js/bg.js
@@ -52,18 +52,18 @@ function getCssValuePrefix() {
   return rtrnVal;
 }
 
-function getColorasHex(a){
+function getColorasHex(a) {
   return rgbToHex(a[0], a[1], a[2]);
 }
 
-function setRotations(rotate){
-  if(rotate){
-    document.getElementById("bg").style.transform = 'rotate('+image_angle+'deg)';
+function setRotations(rotate) {
+  if (rotate) {
+    document.getElementById("bg").style.transform = 'rotate(' + image_angle + 'deg)';
     document.getElementById("bg").style.height = '200vh';
     document.getElementById("bg").style.width = '300vw';
     document.getElementById("bg").style.top = '-50vh';
     document.getElementById("bg").style.left = '-50vh';
-  }else{
+  } else {
     document.getElementById("bg").style.transform = 'rotate(0deg)';
     document.getElementById("bg").style.height = '100vh';
     document.getElementById("bg").style.width = '100vw';
@@ -71,29 +71,32 @@ function setRotations(rotate){
     document.getElementById("bg").style.left = '00vh';
   }
 }
-function placeMainImage(primaryImage){
-  if(image_place == 'framed'){
+function placeMainImage(primaryImage) {
+  if (image_place == 'framed') {
     document.getElementById("canvas").style.display = 'block';
     document.getElementById("canvas").src = primaryImage;
     setRotations(false)
-  }else if(image_place == 'background'){
+  } else if (image_place == 'background') {
     document.getElementById("canvas").style.display = 'none';
-    document.getElementById("bg").style.backgroundImage = 'url('+primaryImage+')';
+    document.getElementById("bg").style.backgroundImage = 'url(' + primaryImage + ')';
     document.getElementById("bg").style.backgroundSize = 'cover';
     setRotations(false)
-  }else if(image_place == 'background-repeat'){
+  } else if (image_place == 'background-repeat') {
     document.getElementById("canvas").style.display = 'none';
-    document.getElementById("bg").style.backgroundImage = 'url('+primaryImage+')';
+    document.getElementById("bg").style.backgroundImage = 'url(' + primaryImage + ')';
     document.getElementById("bg").style.backgroundSize = 'auto';
     setRotations(true)
-    
+
   }
 }
 
-function setTexts(title, artist, objectDate, objectURL){
+function setTexts(title, artist, objectDate, objectURL) {
   document.getElementById("title").innerHTML = title;
-  var desc = artist;
-  if (objectDate != "") {
+  var desc = '';
+  if (artist !== undefined) {
+    desc += artist;
+  }
+  if (objectDate != "" && objectDate !== undefined) {
     desc += ' (' + objectDate + ') ';
   }
   if (objectURL != "") {
@@ -102,74 +105,74 @@ function setTexts(title, artist, objectDate, objectURL){
   document.getElementById("author").innerHTML = desc;
 }
 
-function getPallete(primaryImage, n, callback){
+function getPallete(primaryImage, n, callback) {
   const colorThief = new ColorThief();
   const img = new Image();
   img.crossOrigin = 'Anonymous';
-  img.src =  primaryImage;
+  img.src = primaryImage;
   img.addEventListener('load', () => {
     var p = colorThief.getPalette(img, n);
     callback(p)
   });
 }
 
-function getPalleteSize(){
-  if(image_place == 'framed'){
+function getPalleteSize() {
+  if (image_place == 'framed') {
     return 4;
-  }else if(image_place == 'background' || image_place == 'background-repeat'){
-    if(endpoint_images == 'pokemon'){
+  } else if (image_place == 'background' || image_place == 'background-repeat') {
+    if (endpoint_images == 'pokemon') {
       return 3;
-    }else{
+    } else {
       return 2;
-    }    
+    }
   }
 }
 
-function setBGColor(p){
-  if(image_place == 'framed'){
+function setBGColor(p) {
+  if (image_place == 'framed') {
     var c = getColorasHex(p[0]);
     var c2 = getColorasHex(p[1]);
     let l1 = getColorLuminosity(c);
     let l2 = getColorLuminosity(c2);
     document.getElementById("bg").style.backgroundImage = getCssValuePrefix() + 'radial-gradient(circle,' + (l1 < l2 ? c2 : c) + ', ' + (l1 < l2 ? c : c2) + ')';
-  }else if(image_place == 'background' || image_place == 'background-repeat'){
-    if(endpoint_images == 'pokemon'){
+  } else if (image_place == 'background' || image_place == 'background-repeat') {
+    if (endpoint_images == 'pokemon') {
       var c = getColorasHex(p[0]);
-      document.getElementById("bg").style.backgroundColor= c;
+      document.getElementById("bg").style.backgroundColor = c;
     }
   }
 }
 
-function blackOrWhite(c,tag,prop){
+function blackOrWhite(c, tag, prop) {
   var light = lightOrDark(c) == 'light' ? true : false;
-  
-  if(prop == 'bg'){
-    document.getElementById(tag).style.backgroundColor = light?"#000":"#fff";
+
+  if (prop == 'bg') {
+    document.getElementById(tag).style.backgroundColor = light ? "#000" : "#fff";
   }
-  if(prop == 'color'){
-    document.getElementById(tag).style.color = light?"#000":"#fff";
-  }  
+  if (prop == 'color') {
+    document.getElementById(tag).style.color = light ? "#000" : "#fff";
+  }
 }
 
-function setStyle(c1,c2){
+function setStyle(c1, c2) {
   var c = getColorasHex(c1);
   document.getElementById("title").style.color = c;
-  blackOrWhite(c,'title','bg')
-  
+  blackOrWhite(c, 'title', 'bg')
+
   var c = getColorasHex(c2);
   document.getElementById("author").style.backgroundColor = c;
-  blackOrWhite(c,'author','color')
+  blackOrWhite(c, 'author', 'color')
 }
 
-function setTextStyles(p){
-  if(image_place == 'framed'){
-    setStyle(p[2],p[3])
-    
-  }else if(image_place == 'background' || image_place == 'background-repeat'){
-    if(endpoint_images == 'pokemon'){
-      setStyle(p[1],p[2])
-    }else{
-      setStyle(p[0],p[1])
+function setTextStyles(p) {
+  if (image_place == 'framed') {
+    setStyle(p[2], p[3])
+
+  } else if (image_place == 'background' || image_place == 'background-repeat') {
+    if (endpoint_images == 'pokemon') {
+      setStyle(p[1], p[2])
+    } else {
+      setStyle(p[0], p[1])
     }
   }
 }
@@ -177,8 +180,8 @@ function setTextStyles(p){
 function setImageAndStyles(primaryImage, title, artist, objectDate, objectURL) {
   placeMainImage(primaryImage)
   setTexts(title, artist, objectDate, objectURL)
-  
-  getPallete(primaryImage, getPalleteSize(), (p) =>{
+
+  getPallete(primaryImage, getPalleteSize(), (p) => {
     lastPallet = p
     setBGColor(p)
     setTextStyles(p)
@@ -189,13 +192,13 @@ function setImageAndStyles(primaryImage, title, artist, objectDate, objectURL) {
 
 function getDataAndDoBG() {
   document.getElementById("overlay").style.display = 'flex';
-  if(lastPallet != ""){
+  if (lastPallet != "") {
     document.getElementById("overlay").style.backgroundColor = getColorasHex(lastPallet[0]);
     document.getElementById("spinner-span").style.backgroundColor = getColorasHex(lastPallet[0]);
-    document.getElementById("spinner").style.background = getCssValuePrefix() + 'linear-gradient(0deg,'+getColorasHex(lastPallet[0])+' 33%, '+getColorasHex(lastPallet[1])+' 100%)';
-    blackOrWhite(getColorasHex(lastPallet[0]),'loading-text','color')
+    document.getElementById("spinner").style.background = getCssValuePrefix() + 'linear-gradient(0deg,' + getColorasHex(lastPallet[0]) + ' 33%, ' + getColorasHex(lastPallet[1]) + ' 100%)';
+    blackOrWhite(getColorasHex(lastPallet[0]), 'loading-text', 'color')
   }
-  const myRequest = new Request('https://awesome-api.vercel.app/api/'+endpoint_images);
+  const myRequest = new Request('https://awesome-api.vercel.app/api/' + endpoint_images);
   fetch(myRequest)
     .then(response => response.json())
     .then(json => {
